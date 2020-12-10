2e056ffcedcc6001ce848bcbafe877df8ce3d7c4
jbubar <jpbubar@gmail.com>
2020-12-10 09:37:54 -0800

    fix uglifier bug to push to heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,14 +4,14 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/routes_util';
 import SplashPage from './splash/splash';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 const App = () => {
     return(
         <div className="app">
             <Switch>
-                <Route exact path="/" component={SplashPage} />
                 <ProtectedRoute exact path="/home" component={HomeContainer} />
+                <AuthRoute exact path="/" component={SplashPage} />
                 <AuthRoute path="/login" component={LoginFormContainer} />
                 <AuthRoute path="/signup" component={SignupFormContainer} />
             </Switch>
