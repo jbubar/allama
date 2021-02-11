b2f9ce5d4bb0bbf8088c2bf77cde94af938c94eb
jbubar <jpbubar@gmail.com>
2021-02-10 20:57:16 -0800

    refactor project sections to be more componentized

--- frontend/components/projects/project_page_container.js
+++ frontend/components/projects/project_page_container.js
@@ -1,24 +1,17 @@
 import { connect } from "react-redux";
 import ProjectPage from "./project_page";
-import { createTask, updateTask, removeTask } from "../../actions/task_actions";
-import { createSection, updateSection, removeSection } from "../../actions/section_actions";
-import { selectProjectTasks, selectProjectSections } from "../../reducers/selectors";
+import { createSection } from "../../actions/section_actions";
+import { selectProjectSections } from "../../reducers/selectors";
 import { withRouter } from 'react-router-dom';
 
 const mapStateToProps = (state, ownProps) => {
     const projectId = ownProps.match.params.projectId;
     return {
-      currentUserId: state.session.currentUserId,
-      users: state.entities.users,
       sections: selectProjectSections(state, projectId),
-      tasks: selectProjectTasks(state, projectId),
       projectId: projectId,
     }
 };
 const mapDispatchToProps = (dispatch) => ({
-  createTask: (task) => dispatch(createTask(task)),
-  updateTask: (task) => dispatch(updateTask(task)),
-  removeTask: (taskId) => dispatch(removeTask(taskId)),
   createSection: (section) => dispatch(createSection(section))
 });
 
---
+++ frontend/components/section/section.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import MutationObserver from 'react-mutation-observer';
+import TaskDate from "../tasks/date";
+import TaskAssignee from "../tasks/assignee";
+import { createTask, updateTask, removeTask } from "../../actions/task_actions";
+import { selectProjectTasks } from "../../reducers/selectors";
+import { updateSection, removeSection } from "../../actions/section_actions";
+
+
+function Section(props) {
+    function addTaskClickHandler(e){
+        const el = $(e.currentTarget)
+        $("<div class='task-legend new-task'><span class='task-title column task'><span class='input new-input' role='textbox' contenteditable></span></span><span class='column'></span><span class='column'></span></div>")
+        .insertBefore(el)
+
+        const newInput = document.querySelector('.new-input')
+        const newTask = document.querySelector('.new-task')
+        const page = document.querySelector('.main-page-container')
+        const focusOnInput = (e) => {
+            e.stopPropagation();
+            newInput.focus();
+        }
+        const onPressEnter = (e) => {
+            if (e.keyCode === 13) {
+                createTask();
+            }
+        }
+        const createTask = (e) => {
+            if (newInput.innerHTML.length > 0){
+                props.createTask({
+                    title: newInput.innerHTML, 
+                    assignee_id: props.currentUserId, 
+                    section_id: el.attr('id')
+                })
+            }
+            newInput.removeEventListener('keydown', onPressEnter)
+            newTask.removeEventListener('click', focusOnInput)
+            page.removeEventListener('click', createTask)
+            newTask.remove()
+        }
+        newInput.focus();
+        newInput.addEventListener('keydown', onPressEnter)
+        newTask.addEventListener('click', focusOnInput)
+        page.addEventListener('click', createTask)
+    }
+    function updateTaskTitle(e){
+        const input = e.target.parentElement
+        const onEnterKey = (e) => {
+            if (e.keyCode === 13) {
+                props.updateTask({title:input.innerHTML, id:input.dataset.taskid});
+
+                input.removeEventListener('keydown', onEnterKey)
+                input.blur();
+            }
+        }
+        input.addEventListener('keydown', onEnterKey)
+    }
+    return (
+        <div key={props.section.id}>
+            <h4 className="section-header">{props.section.name}</h4>
+            <div>
+                { props.tasks ? ( props.tasks.filter(task => task.sectionId === props.section.id).map(task => {
+                    if (task) return <div key={task.id} className="task-legend">
+                        
+                        <span className="task-title column task">
+                            <AiOutlineCheckCircle className='check-icon' onClick={() => props.removeTask(task.id)}/>
+                            <MutationObserver onContentChange={updateTaskTitle}>
+                                <span
+                                    data-taskid={task.id}
+                                    className="input" 
+                                    role="textbox" 
+                                    contentEditable
+                                >{task.title}</span>
+                            </MutationObserver>
+                        </span>
+                        <span className="column">
+                            <TaskAssignee task={task} inputType="task"/>
+                        </span>
+                        <span className="column">
+                            <TaskDate task={task}/>
+                        </span>
+                    </div>
+                })) : (
+                    <p>No tasks have been made for this project yet!</p>
+                )}
+                <div className="add-task" onClick={addTaskClickHandler} id={props.section.id}>Add task...</div>
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = (state, ownProps) => ({
+    currentUserId: state.session.currentUserId,
+    section: ownProps.section,
+    tasks: selectProjectTasks(state, ownProps.section.projectId)
+});
+const mapDispatchToProps = (dispatch) => ({
+  createTask: (task) => dispatch(createTask(task)),
+  updateTask: (task) => dispatch(updateTask(task)),
+  removeTask: (taskId) => dispatch(removeTask(taskId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Section);
