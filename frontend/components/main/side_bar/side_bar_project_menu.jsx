91394cb906c8c0cf604d239a13d47fea81f7275d
jbubar <jpbubar@gmail.com>
2021-02-13 01:06:47 -0800

    connecting project destroy

--- frontend/components/main/side_bar/side_bar_project_menu.jsx
+++ frontend/components/main/side_bar/side_bar_project_menu.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {BsPencil, BsLink45Deg} from 'react-icons/bs';
-import { openProjectModal } from "../../../actions/ui_actions.js"
+import { openProjectModal } from "../../../actions/ui_actions";
+import {removeProject} from "../../../actions/project_actions";
+import {withRouter} from 'react-router-dom';
 
 
-function ProjectMenu({ projectId, openProjectModal, closeMenu }) {
+function ProjectMenu({ projectId, openProjectModal, closeMenu, removeProject, match, history }) {
     return (
         <div>
             <div className="menu-item" onClick={() => {openProjectModal(projectId); closeMenu()}}>
                 <span className="menu-sub-icon"><BsPencil/></span>
                 <div>Edit Project</div>
             </div>
-            <div className="menu-item" onClick={() => {window.navigator.clipboard.writeText(`https://allama.herokuapp.com/projects/${projectId}`)}}>
+            <div 
+                className="menu-item" 
+                onClick={() => {
+                    window.navigator.clipboard.writeText(`https://allama.herokuapp.com/projects/${projectId}`)
+                    closeMenu()
+                }}>
                 <span className="menu-sub-icon"><BsLink45Deg/></span>
                 <div>Copy Project Link</div>
             </div>
             <div className="dividing-line"></div>
-            <div className="menu-item delete-btn">
+            <div 
+                className="menu-item delete-btn" 
+                onClick={()=>{
+                    removeProject()
+                    if(match.params.projectId == projectId){history.push('/home')}
+                }}>
                 Delete Project
             </div>
         </div>
@@ -28,7 +40,8 @@ const mapStateToProps = (state, ownProps) => ({
 })
 const mapDispatchToProps = (dispatch, ownProps) => ({
     openProjectModal: (projectId) => dispatch(openProjectModal(projectId)),
+    removeProject: () => dispatch(removeProject(ownProps.projectId)),
     closeMenu: ownProps.closeMenu
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProjectMenu)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProjectMenu))
